0913ac77c9a6f39dc013e47865f7ed7b63d2a655
prashantparajuli <usertest3r@gmail.com>
2024-09-26 02:02:53 +0545

    Project initialization

---
+++ ProductService/src/main/java/com/codefest/ecommercespringboot/model/Product.java
@@ -0,0 +1,13 @@
+package com.codefest.ecommercespringboot.model;
+
+@Entity
+public class Product {
+    private int id;
+    private String title;
+    private BigDecimal price;
+    private String description;
+    private String image_url;
+    private Category category_id;
+    private 
+
+}
---
+++ ProductService/src/test/java/com/codefest/ecommercespringboot/EcommerceSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefest.ecommercespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
